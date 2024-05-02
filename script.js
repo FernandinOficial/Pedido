3be74ea9d3d23c302b0c8644f1c3f8330d841790
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-01 22:59:05 -0300

    Alterando Script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-<link src="index.html"></link>
+
+// Função para alterar o título
+function changeTitle() {
+    document.title = "Pedido Irrecusável";
+}
+
+// Função para restaurar o título original
+function restoreTitle() {
+    document.title = "Volte aqui ;(";
+}
+
+// Adiciona um evento para quando a janela ganhar foco
+window.addEventListener('focus', changeTitle);
+
+// Adiciona um evento para quando a janela perder foco
+window.addEventListener('blur', restoreTitle);
+
+//recarrega a página
+window.addEventListener('focus', function() {
+    location.reload();
+});
