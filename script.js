5c4d47760da59e932bde191da895a23b63969ebf
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-04 23:46:37 -0300

    Partes Finalizadas (necessita atualizações)

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ window.addEventListener('blur', restoreTitle);
 window.addEventListener('focus', function() {
     location.reload();
 });
+
+
+
+//Seção de incorporação do código Button ser calculado para página
+
+function mostrarResposta() {
+    document.getElementById('resposta').style.display = 'block';
+    document.getElementById('resposta').innerHTML = 'Sim! Eu aceito, meu amor. ❤️';
+}
+
+function mudarPosicao() {
+    const buttonNao = document.getElementById('nao');
+    const randomX = Math.floor(Math.random() * window.innerWidth);
+    const randomY = Math.floor(Math.random() * window.innerHeight);
+    buttonNao.style.left = `${randomX}px`;
+    buttonNao.style.top = `${randomY}px`;
+}
+// Redireciona para confirmacao.html
+function mostrarResposta() {
+    window.location.href = "confirmacao.html";
+}
